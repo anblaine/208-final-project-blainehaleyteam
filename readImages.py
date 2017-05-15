3d0d5414a06313e7238c201f19743486b80d58cd
Alex Blaine <Alex Blaine>
2017-05-15 14:48:31 -0700

    oops

--- readImages.py
+++ readImages.py
@@ -1,3 +1,21 @@
+def readAll(directory, filetype, color_mode):
+    """
+    Returns a list of all images in the directory in the format of readImage()
+
+    Input:  directory where images are stored,
+            filetype (suggested to use .png)
+            color mode (see readImage)
+    """
+    import glob
+
+    images = []
+
+    for filename in glob.glob(directory + '/*' + filetype):
+        images.append(readImage(filename, color_mode))
+
+    return images
+
+
 def readImage(filename, color_mode):
     """
     Creates a numpy array for an image
@@ -14,19 +32,3 @@ def readImage(filename, color_mode):
     image = misc.imread(filename, mode=color_mode)
     A = np.asarray(image)
     return A
-
-
-def readAll(directory, color_mode):
-    """
-    Returns a list of all images in the directory in the format of readImage()
-
-    Input:  directory where images are stored,
-            color mode (see readImage)
-    """
-    from glob import glob
-
-    images = []
-    for filename in glob('./Images/*.png'):
-        images.append(readImage.readImage(filename, "RGB"))
-
-    return images
